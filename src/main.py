8b4a97311f35c7d447973dd13f2811935b104286
szykol <szymonkolton@gmail.com>
2019-11-24 13:05:43 +0100

    Add jwt example

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
 from sanic import Sanic
 from sanic.response import json
+import jwt
 
 app = Sanic()
 
-@app.route("/")
+@app.route("/", methods=['POST'])
 async def test(request):
-    return json({"hello": "world"})
+    print(request.body)
+    try:
+        d = jwt.decode(request.body, 'serial', algorithm='HS256')
+        return json({"status": 200, "msg": "Success"})
+    except jwt.exceptions.InvalidSignatureError:
+        return json({"status": 400, "msg": "User not authenticated"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
